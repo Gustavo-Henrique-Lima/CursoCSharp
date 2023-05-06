78b9ddf9663fa7bf41e8e06840aef82bc77121dd
Gustavo Henrique <gustavohgustavo@hotmail.com>
2023-05-06 18:54:17 -0300

    Tipos primitivos em C#

--- Program.cs
+++ Program.cs
@@ -11,15 +11,21 @@ namespace CursoC
             int n3=2147483647;//Min -2147483648
             long n4=9223372036854775806L;//Min -9223372036854775808
             byte n5=255;//Min 0
+            float n6=4.5f;
+            double n7=4.5;
             bool completo=true;
             char genero='M';
+            string nome="Gustavo";//Não é considerado primitivo em C#
             Console.WriteLine(n1);
             Console.WriteLine(n2);
             Console.WriteLine(n3);
             Console.WriteLine(n4);
             Console.WriteLine(n5);
+            Console.WriteLine(n6);
+            Console.WriteLine(n7);
             Console.WriteLine(completo);
             Console.WriteLine(genero);
+            Console.WriteLine(nome);
         }
     }
 }
